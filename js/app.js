555592a39bd18896d8aa7ca6ab79325bd5e2d46b
Danny <info@dannybcreative.com>
2020-03-20 14:18:28 -0700

    Ready to go!

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector('#nav-toggle'),
+  navMenu = document.querySelector('.nav-menu');
+
+// Event Listener
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
+
+window.addEventListener('resize', () => {
+  const winWidth = window.innerWidth;
+  if (winWidth > 992) {
+    navMenu.classList.remove('show');
+  }
+
+});
